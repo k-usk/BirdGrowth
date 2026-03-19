f096176c5cfbc6f2ecaa671b34f69ee5859b2041
k-usk <onetwosan+gh@gmail.com>
2026-03-19 22:54:31 +0900

    Initial Commit

---
+++ BirdGrowth/BirdGrowthApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirdGrowthApp.swift
+//  BirdGrowth
+//
+//  Created by Yusuke on 2026/03/19.
+//
+
+import SwiftUI
+
+@main
+struct BirdGrowthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BirdGrowth/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BirdGrowth
+//
+//  Created by Yusuke on 2026/03/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
